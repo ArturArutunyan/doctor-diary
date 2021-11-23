d7c619c038a74792ac8da9cc9418f4747bd2fdfd
Artur Arutiunian <artur@linkse.io>
2021-11-23 14:11:01 +0400

    Added missed page

---
+++ DoctorDiary/DoctorDiary/Views/Reminders/RemindersPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using DoctorDiary.ViewModels.Reminders;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DoctorDiary.Views.Reminders
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RemindersPage : ContentPage
+    {
+        private readonly RemindersViewModel _remindersViewModel;
+
+        public RemindersPage()
+        {
+            InitializeComponent();
+            BindingContext = _remindersViewModel = new RemindersViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _remindersViewModel.OnAppearing();
+        }
+    }
+}
