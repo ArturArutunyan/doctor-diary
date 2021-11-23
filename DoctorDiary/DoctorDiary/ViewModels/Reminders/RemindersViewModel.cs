eab2e96fa87378ec97a0e121549a17f303bd08ef
Artur Arutiunian <artur@linkse.io>
2021-11-23 14:10:23 +0400

    Added reminder page

--- DoctorDiary/DoctorDiary/ViewModels/Reminders/RemindersViewModel.cs
+++ DoctorDiary/DoctorDiary/ViewModels/Reminders/RemindersViewModel.cs
@@ -1,16 +1,76 @@
-﻿using DoctorDiary.Services.Reminders;
-using Microsoft.Extensions.DependencyModel;
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using DoctorDiary.Models.Reminders;
+using DoctorDiary.Services.Reminders;
+using MvvmHelpers;
+using MvvmHelpers.Commands;
 using Xamarin.Forms;
 
 namespace DoctorDiary.ViewModels.Reminders
 {
     public class RemindersViewModel : BaseViewModel
     {
+        private Reminder _selectedReminder;
         private readonly IReminderAppService _reminderAppService;
 
+        public Reminder SelectedReminder
+        {
+            get => _selectedReminder;
+            set
+            {
+                SetProperty(ref _selectedReminder, value);
+                OnReminderSelected(value);
+            }
+        }
+        
+        public ObservableRangeCollection<Reminder> Reminders { get; }
+        
+        public AsyncCommand LoadRemindersCommand { get; }
+
+        public AsyncCommand<Reminder> ReminderTapped { get; }
+        
         public RemindersViewModel()
         {
             _reminderAppService = DependencyService.Get<IReminderAppService>();
+            
+            Title = "Напоминания";
+            Reminders = new ObservableRangeCollection<Reminder>();
+            LoadRemindersCommand = new AsyncCommand(LoadReminders);
+            ReminderTapped = new AsyncCommand<Reminder>(OnReminderSelected);
+        }
+
+        private async Task LoadReminders()
+        {
+            IsBusy = true;
+
+            try
+            {
+                Reminders.Clear();
+                
+                var reminders = await _reminderAppService.GetLastActiveReminders(0, 10);
+                
+                Reminders.AddRange(reminders);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        public void OnAppearing()
+        {
+            IsBusy = true;
+            SelectedReminder = null;
+        }
+
+        private async Task OnReminderSelected(Reminder reminder)
+        {
+            await Shell.Current.GoToAsync(reminder.NavigationLinkOnClick);
         }
     }
 }
