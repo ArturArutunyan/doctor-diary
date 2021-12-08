f41c86681863e31a37ece45e72b630c60e11c34a
Artur Arutiunian <artur@linkse.io>
2021-12-08 19:42:39 +0400

    Added BusinessException

---
+++ DoctorDiary/DoctorDiary.Shared/Exceptions/BusinessException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DoctorDiary.Shared.Exceptions
+{
+    public class BusinessException : Exception
+    {
+        public BusinessException(string message) : base(message)
+        {
+            
+        }
+    }
+}
