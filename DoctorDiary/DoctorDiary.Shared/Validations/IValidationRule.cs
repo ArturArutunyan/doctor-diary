8d5037c60585f4bced8a453f6ef6702f564e7e1f
Artur Arutiunian <artur@linkse.io>
2022-01-09 17:40:09 +0400

    Add IValidationRule

---
+++ DoctorDiary/DoctorDiary.Shared/Validations/IValidationRule.cs
@@ -0,0 +1,9 @@
+﻿namespace DoctorDiary.Shared.Validations
+{
+    public interface IValidationRule<T>
+    {
+        string ValidationMessage { get; }
+        
+        bool Check(T value);
+    }
+}
