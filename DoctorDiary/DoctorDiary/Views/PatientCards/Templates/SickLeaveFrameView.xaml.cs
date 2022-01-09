76fd7ceda50dfba94b6c51b4ba3b0af11ad4409f
Artur Arutiunian <artur@linkse.io>
2022-01-09 19:10:00 +0400

    Moved sick leave frame to view

---
+++ DoctorDiary/DoctorDiary/Views/PatientCards/Templates/SickLeaveFrameView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DoctorDiary.Views.PatientCards.Templates
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SickLeaveFrameView : ContentView
+    {
+        public SickLeaveFrameView()
+        {
+            InitializeComponent();
+        }
+    }
+}
