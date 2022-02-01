b031fc6474b0d696a01e358dda42ccd68eab8411
Artur Arutiunian <artur@linkse.io>
2022-02-02 00:07:28 +0400

    Reworked NewPatientCardPage

--- DoctorDiary/DoctorDiary/ViewModels/PatientCards/NewPatientCardViewModel.cs
+++ DoctorDiary/DoctorDiary/ViewModels/PatientCards/NewPatientCardViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using DoctorDiary.Models.PatientCards.ValueObjects;
 using DoctorDiary.Services.PatientCards;
 using DoctorDiary.Shared.Validations;
+using DoctorDiary.ViewModels.PatientCards.Validations;
 using Xamarin.CommunityToolkit.ObjectModel;
 using Xamarin.Forms;
 
@@ -11,80 +13,142 @@ namespace DoctorDiary.ViewModels.PatientCards
 {
     public class NewPatientCardViewModel : BaseViewModel
     {
-        private string _lastName;
-        private string _firstName;
-        private ValidatableObject<string> _patronymic;
+        private ValidatableObject<string> _lastName;
+        private ValidatableObject<string> _firstName;
+        private string _patronymic;
         private string _city;
         private string _street;
         private string _apartment;
         private string _house;
         private string _entrance;
         private string _floor;
-        private string _birthday;
-        private string _snils;
+        private ValidatableObject<string> _birthday;
+        private ValidatableObject<string> _snils;
         private string _description;
-        private string _phoneNumber;
+        private ValidatableObject<string> _phoneNumber;
         private string _gender;
-        private string _insurancePolicy;
+        private ValidatableObject<string> _insurancePolicy;
         private string _placeOfWork;
         private string _employmentPosition;
-        private int _precinct;
+        private int? _precinct;
 
         private readonly IPatientCardAppService _patientCardAppService;
 
         public NewPatientCardViewModel()
         {
             _patientCardAppService = DependencyService.Get<IPatientCardAppService>();
-            _patronymic = new ValidatableObject<string>();
+            
+            _lastName = new ValidatableObject<string>();
+            _firstName = new ValidatableObject<string>();
+            _birthday = new ValidatableObject<string>();
+            _snils = new ValidatableObject<string>();
+            _phoneNumber = new ValidatableObject<string>();
+            _insurancePolicy = new ValidatableObject<string>();
+
             BirthdayDatePicker = null;
-            SaveCommand = new AsyncCommand(OnSave);
             CancelCommand = new AsyncCommand(OnCancel);
-            PropertyChanged +=
-                (_, __) => SaveCommand.RaiseCanExecuteChanged();
             
             AddValidations();
         }
 
         private void AddValidations()
         {
-            _patronymic.Validations.Add(new IsNotNullOrEmptyRule<string>
+            _firstName.Validations.Add(new IsNotNullOrEmptyRule<string>
             {
-                ValidationMessage = "A patronymic is required."
+                ValidationMessage = "Поле имя обязательное"
             });
+            _lastName.Validations.Add(new IsNotNullOrEmptyRule<string>
+            {
+                ValidationMessage = "Поле фамилия обязательное"
+            });
+            _birthday.Validations.Add(new DateTimeAsStringRule());
+            _snils.Validations.AddRange(new List<IValidationRule<string>>()
+            {
+                new SnilsLengthRule(),
+                new SnilsContainsOnlyDigitsRule()
+            });
+            _insurancePolicy.Validations.Add(new InsurancePolicyRuleLength());
+            _phoneNumber.Validations.Add(new PhoneNumberRule());
         }
         
         
-        private bool Validate()
+        internal bool Validate()
         {
-            var isValidPatronymic = ValidatePatronymic();
-
-            return isValidPatronymic;
+            var birthdayIsValid = ValidateBirthday();
+            var firstNameIsValid = ValidateFirstName();
+            var lastNameIsValid = ValidateLastName();
+            var snilsIsValid = ValidateSnils();
+            var phoneNumberIsValid = ValidatePhoneNumber();
+            var insurancePolicyIsValid = ValidateInsurancePolicy();
+            
+            return birthdayIsValid &&
+                   firstNameIsValid &&
+                   lastNameIsValid &&
+                   snilsIsValid &&
+                   phoneNumberIsValid &&
+                   insurancePolicyIsValid;
         }
 
-        private bool ValidatePatronymic()
+        
+        private bool ValidateLastName()
         {
-            return _patronymic.Validate();
+            return _lastName.Validate();
+        }
+
+        private bool ValidateFirstName()
+        {
+            return _firstName.Validate();
+        }
+
+        private bool ValidateBirthday()
+        {
+            return _birthday.Validate();
+        }
+
+        private bool ValidateSnils()
+        {
+            return _snils.Validate();
+        }
+
+        private bool ValidateInsurancePolicy()
+        {
+            return _insurancePolicy.Validate();
+        }
+
+        private bool ValidatePhoneNumber()
+        {
+            return _phoneNumber.Validate();
         }
 
         public AsyncCommand SaveCommand { get; }
 
         public AsyncCommand CancelCommand { get; }
 
-        public ICommand ValidatePatronymicCommand => new Command(() => ValidatePatronymic());
+        public ICommand ValidateLastNameCommand => new Command(() => ValidateLastName());
 
-        public string LastName
+        public ICommand ValidateFirstNameCommand => new Command(() => ValidateFirstName());
+
+        public ICommand ValidateBirthdayCommand => new Command(() => ValidateBirthday());
+
+        public ICommand ValidateSnilsCommand => new Command(() => ValidateSnils());
+
+        public ICommand ValidateInsurancePolicyCommand => new Command(() => ValidateInsurancePolicy());
+
+        public ICommand ValidatePhoneNumberCommand => new Command(() => ValidatePhoneNumber());
+
+        public ValidatableObject<string> LastName
         {
             get => _lastName;
             set => SetProperty(ref _lastName, value);
         }
 
-        public string FirstName
+        public ValidatableObject<string> FirstName
         {
             get => _firstName;
             set => SetProperty(ref _firstName, value);
         }
 
-        public ValidatableObject<string> Patronymic
+        public string Patronymic
         {
             get => _patronymic;
             set => SetProperty(ref _patronymic, value);
@@ -126,7 +190,7 @@ namespace DoctorDiary.ViewModels.PatientCards
             set => SetProperty(ref _floor, value);
         }
         
-        public string Birthday 
+        public ValidatableObject<string> Birthday 
         {
             get => _birthday;
             set => SetProperty(ref _birthday, value);
@@ -134,7 +198,7 @@ namespace DoctorDiary.ViewModels.PatientCards
 
         public DateTime? BirthdayDatePicker { get; set; }
 
-        public string Snils
+        public ValidatableObject<string> Snils
         {
             get => _snils;
             set => SetProperty(ref _snils, value);
@@ -146,7 +210,7 @@ namespace DoctorDiary.ViewModels.PatientCards
             set => SetProperty(ref _description, value);
         }
         
-        public string PhoneNumber
+        public ValidatableObject<string> PhoneNumber
         {
             get => _phoneNumber;
             set => SetProperty(ref _phoneNumber, value);
@@ -158,7 +222,7 @@ namespace DoctorDiary.ViewModels.PatientCards
             set => SetProperty(ref _gender, value);
         }
         
-        public string InsurancePolicy
+        public ValidatableObject<string> InsurancePolicy
         {
             get => _insurancePolicy;
             set => SetProperty(ref _insurancePolicy, value);
@@ -176,7 +240,7 @@ namespace DoctorDiary.ViewModels.PatientCards
             set => SetProperty(ref _employmentPosition, value);
         }
         
-        public int Precinct
+        public int? Precinct
         {
             get => _precinct;
             set => SetProperty(ref _precinct, value);
@@ -189,25 +253,25 @@ namespace DoctorDiary.ViewModels.PatientCards
             await Shell.Current.GoToAsync("..");
         }
 
-        private async Task OnSave()
+        internal async Task Save()
         {
             await _patientCardAppService.CreateAsync(
-                firstName: FirstName,
-                lastName: LastName,
-                patronymic: Patronymic.Value,
+                firstName: FirstName.Value,
+                lastName: LastName.Value,
+                patronymic: Patronymic,
                 address: new Address(city: City, street: Street, apartment: Apartment, house: House, entrance: Entrance, floor: Floor),
-                birthday: !string.IsNullOrEmpty(Birthday) 
-                    ? DateTime.ParseExact(s: Birthday, format: "dd.MM.yyyy", provider: null)
+                birthday: !string.IsNullOrEmpty(Birthday.Value) 
+                    ? DateTime.ParseExact(s: Birthday.Value, format: "dd.MM.yyyy", provider: null)
                     : (DateTime?)null,
-                snils: string.IsNullOrEmpty(Snils) 
+                snils: string.IsNullOrEmpty(Snils.Value) 
                     ? Models.PatientCards.ValueObjects.Snils.Empty() 
-                    : new Snils(Snils),
+                    : new Snils(Snils.Value),
                 description: Description,
-                phoneNumber: new PhoneNumber(PhoneNumber),
+                phoneNumber: new PhoneNumber(PhoneNumber.Value),
                 gender: Gender,
-                insurancePolicy: string.IsNullOrEmpty(InsurancePolicy) 
+                insurancePolicy: string.IsNullOrEmpty(InsurancePolicy.Value) 
                     ? Models.PatientCards.ValueObjects.InsurancePolicy.Empty() 
-                    : new InsurancePolicy(InsurancePolicy),
+                    : new InsurancePolicy(InsurancePolicy.Value),
                 placeOfWork: PlaceOfWork,
                 employmentPosition: EmploymentPosition,
                 precinct: Precinct);
